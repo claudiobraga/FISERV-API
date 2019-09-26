2c0af2fee9782847f57868c2b167843e6855cbbf
Cláudio Braga <cjbdeveloper@gmail.com>
2019-09-26 23:20:00 +0100

    Update OpportunityRepository.java

--- OpportunityRepository.java
+++ OpportunityRepository.java
@@ -1,5 +1,3 @@
-//Interface resposnsável por toda operação de CRUD na BD
-//Porque herda os métodos do JpaRepository
 package com.claudiobraga.fiservnenaghopportunity.repository;
 
 import java.util.Optional;
@@ -9,8 +7,6 @@ import com.claudiobraga.fiservnenaghopportunity.model.Opportunity;
 
 public interface OpportunityRepository extends JpaRepository<Opportunity, Long>{
 	
-	//Implementar um método diferente que não foi ainda implementado pelo o JPA
-	//Que procura pela descrição e nome da companhia. É preciso passar os paramêtros
 	Optional<Opportunity> findByDescriptionAndNameOfCompany(String description, String nameOfCompany);
 
 }
